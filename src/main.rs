27ea46348a76d775a3a5ef6954c73838cf156389
Martin Billinger <mbillingr@users.noreply.github.com>
2023-12-29 22:49:50 +0100

    factor out unification

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ mod kinds;
 mod substitutions;
 mod types;
 mod types_specific;
+mod unification;
 
 use crate::kinds::{HasKind, Kind};
 use crate::substitutions::{Subst, Types};
 use crate::types::{Type, Tyvar};
+use crate::unification::{matches, mgu};
 use crate::Pred::IsIn;
 use std::fmt::{Debug, Formatter};
 use std::iter::once;
@@ -106,60 +108,6 @@ fn eq_intersect<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
         }
     }
     out
-}
-
-fn mgu(a: &Type, b: &Type) -> Result<Subst> {
-    use Type::*;
-    match (a, b) {
-        (TApp(app1), TApp(app2)) => {
-            let s1 = mgu(&app1.0, &app2.0)?;
-            let s2 = mgu(&s1.apply(&app1.1), &s1.apply(&app2.1))?;
-            Ok(s2.compose(&s1))
-        }
-
-        (TVar(u), t) => var_bind(u, t),
-
-        (t, TVar(u)) => var_bind(u, t),
-
-        (TCon(tc1), TCon(tc2)) if tc1 == tc2 => Ok(Subst::null_subst()),
-
-        _ => Err(format!("types do not unify: {a:?}, {b:?}"))?,
-    }
-}
-
-fn var_bind(u: &Tyvar, t: &Type) -> Result<Subst> {
-    if let Type::TVar(v) = t {
-        if u == v {
-            return Ok(Subst::null_subst());
-        }
-    }
-
-    if t.tv().contains(u) {
-        Err("occurs check failed")?
-    }
-
-    if u.kind() != t.kind() {
-        Err("kinds do not match")?
-    }
-
-    Ok(Subst::single(u.clone(), t.clone()))
-}
-
-fn matches(a: &Type, b: &Type) -> Result<Subst> {
-    use Type::*;
-    match (a, b) {
-        (TApp(app1), TApp(app2)) => {
-            let sl = matches(&app1.0, &app2.0)?;
-            let sr = matches(&app1.1, &app2.1)?;
-            sl.merge(&sr)
-        }
-
-        (TVar(u), t) if u.kind() == t.kind() => Ok(Subst::single(u.clone(), t.clone())),
-
-        (TCon(tc1), TCon(tc2)) if tc1 == tc2 => Ok(Subst::null_subst()),
-
-        _ => Err("types do not match")?,
-    }
 }
 
 #[derive(Clone, PartialEq)]
---
+++ src/unification.rs
@@ -0,0 +1,57 @@
+use crate::kinds::HasKind;
+use crate::substitutions::{Subst, Types};
+use crate::types::{Type, Tyvar};
+
+pub fn mgu(a: &Type, b: &Type) -> crate::Result<Subst> {
+    use Type::*;
+    match (a, b) {
+        (TApp(app1), TApp(app2)) => {
+            let s1 = mgu(&app1.0, &app2.0)?;
+            let s2 = mgu(&s1.apply(&app1.1), &s1.apply(&app2.1))?;
+            Ok(s2.compose(&s1))
+        }
+
+        (TVar(u), t) => var_bind(u, t),
+
+        (t, TVar(u)) => var_bind(u, t),
+
+        (TCon(tc1), TCon(tc2)) if tc1 == tc2 => Ok(Subst::null_subst()),
+
+        _ => Err(format!("types do not unify: {a:?}, {b:?}"))?,
+    }
+}
+
+fn var_bind(u: &Tyvar, t: &Type) -> crate::Result<Subst> {
+    if let Type::TVar(v) = t {
+        if u == v {
+            return Ok(Subst::null_subst());
+        }
+    }
+
+    if t.tv().contains(u) {
+        Err("occurs check failed")?
+    }
+
+    if u.kind() != t.kind() {
+        Err("kinds do not match")?
+    }
+
+    Ok(Subst::single(u.clone(), t.clone()))
+}
+
+pub fn matches(a: &Type, b: &Type) -> crate::Result<Subst> {
+    use Type::*;
+    match (a, b) {
+        (TApp(app1), TApp(app2)) => {
+            let sl = matches(&app1.0, &app2.0)?;
+            let sr = matches(&app1.1, &app2.1)?;
+            sl.merge(&sr)
+        }
+
+        (TVar(u), t) if u.kind() == t.kind() => Ok(Subst::single(u.clone(), t.clone())),
+
+        (TCon(tc1), TCon(tc2)) if tc1 == tc2 => Ok(Subst::null_subst()),
+
+        _ => Err("types do not match")?,
+    }
+}
