205fe4f8cce3fd3ba61bb8acff4a8c5f7c40ac80
Martin Billinger <mbillingr@users.noreply.github.com>
2023-12-29 23:20:55 +0100

    combine language specifics

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,16 @@ mod classes;
 mod kinds;
 mod predicates;
 mod qualified;
+mod specifics;
 mod substitutions;
 mod types;
-mod types_specific;
 mod unification;
 
 use crate::classes::{ClassEnv, EnvTransformer};
 use crate::kinds::{HasKind, Kind};
 use crate::predicates::{match_pred, mgu_pred};
 use crate::qualified::Qual;
+use crate::specifics::{add_core_classes, add_num_classes};
 use crate::substitutions::{Subst, Types};
 use crate::types::{Type, Tyvar};
 use crate::unification::{matches, mgu};
@@ -115,42 +116,6 @@ fn eq_intersect<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
         }
     }
     out
-}
-
-fn add_core_classes() -> EnvTransformer {
-    use EnvTransformer as ET;
-    ET::add_class("Eq".into(), vec![])
-        .compose(ET::add_class("Ord".into(), vec!["Eq".into()]))
-        .compose(ET::add_class("Show".into(), vec![]))
-        .compose(ET::add_class("Read".into(), vec![]))
-        .compose(ET::add_class("Bounded".into(), vec![]))
-        .compose(ET::add_class("Enum".into(), vec![]))
-        .compose(ET::add_class("Functor".into(), vec![]))
-        .compose(ET::add_class("Monad".into(), vec![]))
-}
-
-fn add_num_classes() -> EnvTransformer {
-    use EnvTransformer as ET;
-    ET::add_class("Num".into(), vec!["Eq".into(), "Show".into()])
-        .compose(ET::add_class(
-            "Real".into(),
-            vec!["Num".into(), "Ord".into()],
-        ))
-        .compose(ET::add_class("Fractional".into(), vec!["Num".into()]))
-        .compose(ET::add_class(
-            "Integral".into(),
-            vec!["Real".into(), "Enum".into()],
-        ))
-        .compose(ET::add_class(
-            "RealFrac".into(),
-            vec!["Real".into(), "Fractional".into()],
-        ))
-        .compose(ET::add_class("Floating".into(), vec!["Fractional".into()]))
-        .compose(ET::add_class(
-            "RealFloat".into(),
-            vec!["RealFrac".into(), "Floating".into()],
-        ))
-        .compose(ET::add_inst(vec![], IsIn("Num".into(), Type::t_int())))
 }
 
 fn overlap(p: &Pred, q: &Pred) -> bool {
--- src/specifics.rs
+++ src/specifics.rs
@@ -1,4 +1,6 @@
+use crate::classes::EnvTransformer;
 use crate::kinds::Kind;
+use crate::predicates::Pred::IsIn;
 use crate::types::{Tycon, Type};
 
 impl Type {
@@ -72,4 +74,41 @@ impl Type {
     pub fn pair(a: Type, b: Type) -> Type {
         Type::tapp(Type::tapp(Type::t_tuple2(), a), b)
     }
+}
+
+pub fn add_core_classes() -> EnvTransformer {
+    use EnvTransformer as ET;
+    ET::add_class("Eq".into(), vec![])
+        .compose(ET::add_class("Ord".into(), vec!["Eq".into()]))
+        .compose(ET::add_class("Show".into(), vec![]))
+        .compose(ET::add_class("Read".into(), vec![]))
+        .compose(ET::add_class("Bounded".into(), vec![]))
+        .compose(ET::add_class("Enum".into(), vec![]))
+        .compose(ET::add_class("Functor".into(), vec![]))
+        .compose(ET::add_class("Monad".into(), vec![]))
+}
+
+pub fn add_num_classes() -> EnvTransformer {
+    use EnvTransformer as ET;
+    let et = ET::add_class("Num".into(), vec!["Eq".into(), "Show".into()])
+        .compose(ET::add_class(
+            "Real".into(),
+            vec!["Num".into(), "Ord".into()],
+        ))
+        .compose(ET::add_class("Fractional".into(), vec!["Num".into()]))
+        .compose(ET::add_class(
+            "Integral".into(),
+            vec!["Real".into(), "Enum".into()],
+        ))
+        .compose(ET::add_class(
+            "RealFrac".into(),
+            vec!["Real".into(), "Fractional".into()],
+        ))
+        .compose(ET::add_class("Floating".into(), vec!["Fractional".into()]))
+        .compose(ET::add_class(
+            "RealFloat".into(),
+            vec!["RealFrac".into(), "Floating".into()],
+        ));
+
+    et.compose(ET::add_inst(vec![], IsIn("Num".into(), Type::t_int())))
 }
